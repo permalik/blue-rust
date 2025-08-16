30b61dc3360871e28296e50696acde33dc4fd7c7
permalik <permalik@protonmail.com>
2025-08-15 21:34:20 -0500

    feat: impl RawLog

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use serde::Deserialize;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -11,4 +13,10 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct RawLog {
+    msg_id: String,
+    content: String,
 }
