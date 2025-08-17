0fe3d59a51135a7184eb07ac8761733343466617
permalik <permalik@protonmail.com>
2025-08-16 19:30:58 -0500

    feat: add service

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,8 @@
 use serde::Deserialize;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 #[derive(Debug, Deserialize)]
 struct RawLog {
     msg_id: String,
+    service: String,
     content: String,
 }
